918b858920c2e89e65eb9bc892e95ef670072004
ninam233 <123406067+ninam233@users.noreply.github.com>
2024-04-18 12:49:40 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from . import views
+from .views import UserRegistrationView
+
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
