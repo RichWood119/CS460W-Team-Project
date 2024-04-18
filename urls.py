b749b42bac48c2b4b74871e1fb1f6824cde9004e
ninam233 <123406067+ninam233@users.noreply.github.com>
2024-04-18 12:39:19 -0400

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,13 @@
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('', views.index, name='index'),
+    path('patients/', views.patient_list, name='patient_list'),
+    path('new_patient/', views.new_patient, name='new_patient'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/doctor/', views.doctor_dashboard, name='doctor_dashboard'),
+    path('dashboard/nurse/', views.nurse_dashboard, name='nurse_dashboard'),
+    path('dashboard/admitting/', views.admitting_staff_dashboard, name='admitting_staff_dashboard'),
+    path('dashboard/billing/', views.billing_staff_dashboard, name='billing_staff_dashboard'),
+]
