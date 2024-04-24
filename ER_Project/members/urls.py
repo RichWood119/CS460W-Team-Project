da0d5eb178335eaa3b3f33658b039d6652da5c6d
RichWood119 <124733536+RichWood119@users.noreply.github.com>
2024-04-24 11:10:50 -0400

    another test commit

--- ER_Project/members/urls.py
+++ ER_Project/members/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('dashboard/billing/', views.billing_staff_dashboard, name='billing_dashboard'),
     path('default/', views.default, name='default'),
 ]
+
+
+#richards test commit
